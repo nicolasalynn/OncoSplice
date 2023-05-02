428fa01d842fec31672bde4cd3947da17a8d82ed
Nico Lynn <nicolasalynn@gmail.com>
2023-05-02 12:30:15 +0300

    more

--- oncosplice/protein_scoring_utils.py
+++ oncosplice/protein_scoring_utils.py
@@ -24,7 +24,7 @@ def generate_report(ref_proteome, var_proteome, missplicing, mutation):
         if W >= len(ref_prot.protein):
             W = len(ref_prot.protein)-1
 
-        smoothed_conservation_vector = smooth_cons_scores([2 ** val for val in ref_prot.conservation_vector], W, W // 2)
+        smoothed_conservation_vector = smooth_cons_scores([2 ** -val for val in ref_prot.conservation_vector], W, W // 2)
 
         deconv_del = calculate_del_penalty(deleted, smoothed_conservation_vector, W, W // 2)
         deconv_ins = calculate_ins_penalty(inserted, smoothed_conservation_vector, W, W // 2)
