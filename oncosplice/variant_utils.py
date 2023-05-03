0cd05c1b52d2f203aacc37806175678cf1b3fb6b
Nico Lynn <nicolasalynn@gmail.com>
2023-05-03 12:59:07 +0300

    Update variant_utils.py

--- oncosplice/variant_utils.py
+++ oncosplice/variant_utils.py
@@ -46,25 +46,24 @@ class Mutation:
 
 def generate_mut_variant(seq: str, indices: list, mut: Mutation):
     offset = 1 if not mut.ref else 0
-    rel_start, rel_end = indices.index(mut.start)+offset, indices.index(mut.start)+offset+len(mut.ref)
 
     check_indices = list(range(mut.start, mut.start + len(mut.ref) + offset))
-    acquired_seq = seq[rel_start:rel_end]
-
     check1 = all([m in indices for m in check_indices])
     if not check1:
         print(f"Mutation {mut} not in indices: {min(indices)} - {max(indices)}.")
         return seq, indices, False
 
+    rel_start, rel_end = indices.index(mut.start)+offset, indices.index(mut.start)+offset+len(mut.ref)
+    acquired_seq = seq[rel_start:rel_end]
     check2 = acquired_seq == mut.ref
     assert check2, f'Reference allele does not match position in SNP. {acquired_seq}, {mut.ref}, {mut.start}'
 
     if len(mut.ref) == len(mut.alt) > 0:
-        new_indices = list(range(mut.start, mut.start + len(mut.ref)))
+        temp_indices = list(range(mut.start, mut.start + len(mut.ref)))
     else:
-        new_indices = [indices[indices.index(mut.start)] + v / 1000 for v in list(range(1, len(mut.alt)+1))]
+        temp_indices = [indices[indices.index(mut.start)] + v / 1000 for v in list(range(1, len(mut.alt)+1))]
 
-    new_indices = indices[:rel_start] + new_indices + indices[rel_end:]
+    new_indices = indices[:rel_start] + temp_indices + indices[rel_end:]
     new_seq = seq[:rel_start] + mut.alt + seq[rel_end:]
 
     assert len(new_seq) == len(new_indices), f'Error in variant modification: {mut}, {len(new_seq)}, {len(new_indices)}'
