3acb953797e7c6a2f4d89776ec9f42626014370b
Nico Lynn <nicolasalynn@gmail.com>
2023-11-08 13:19:58 +0200

    done

--- oncosplice/Gene.py
+++ oncosplice/Gene.py
@@ -84,7 +84,7 @@ class EmptyGene:
     def develop_proteome(self):
         proteome = {}
         for tid in self.transcripts.keys():
-            if 'TIS' in self.transcripts[tid].keys() and self.transcripts[tid]['transcript_type'] == 'protein_coding' and 'Ensembl_canonical' in self.trasncripts[tid]['tag']:
+            if 'TIS' in self.transcripts[tid].keys() and self.transcripts[tid]['transcript_type'] == 'protein_coding' and 'Ensembl_canonical' in self.transcripts[tid]['tag']:
                 proteome[tid] = self.develop_protein(tid)
         return proteome
 
